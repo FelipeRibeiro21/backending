700d4bcb340c34b2964a89a3e3f49ef1fd359907
FelipeRibeiro21 <felipe.ribeiro.senai404@gmail.com>
2024-08-21 15:19:33 -0300

    Add files via upload

---
+++ exer6.php
@@ -0,0 +1,25 @@
+<?php
+
+$numero = 50;
+$contador = 0;
+//Dados
+
+if ($contador >= 0){ //Maior ou menor que zero
+    while($contador <=$numero){//Numero tem que ser maior qye o contador
+        if($contador % 2 == 0){ //Se é par ou nao
+            echo"$contador<br>";
+
+        }
+        $contador++;
+    
+    }    
+}
+    else{
+        echo "Numero Invalido";
+    }
+   
+
+
+ 
+       
+    
