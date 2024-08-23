bf02f76a8b54dd35eee44067718679ab6a21d735
FelipeRibeiro21 <felipe.ribeiro.senai404@gmail.com>
2024-08-23 09:46:51 -0300

    Add files via upload

---
+++ Php/exer9.php
@@ -0,0 +1,10 @@
+<?php
+
+for( $i = 1; $i <= 50; $i++) { //Usando o for
+    $raiz = sqrt($i); //Como usar a raiz quadrada
+
+    $valorfinal =   round($raiz, 2);
+      
+    echo "A raiz quadrada de $i é de $valorfinal <br>";
+    
+}
