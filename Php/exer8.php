64da2dff825beb110304e9da0ba11a954fe3da53
FelipeRibeiro21 <felipe.ribeiro.senai404@gmail.com>
2024-08-23 09:36:19 -0300

    Add files via upload

---
+++ Php/exer8.php
@@ -0,0 +1,13 @@
+<?php
+
+$soma = 0; //Variavel
+
+for ($i = 1; $i <= 100; $i++) { //Usando e for
+    $soma += $i;   // A soma sera o valor inicial mais a soma 
+
+    
+}
+echo "A soma de todos os números de 1 a 100 sera: $soma <br>";
+
+
+
---
+++ Php/exer9.php
@@ -0,0 +1,6 @@
+<?php
+
+for( $i = 1; $i <= 50; $i++) { //Usando o for
+    $raiz = sqrt($i); //Como usar a raiz quadrada
+echo "A raiz quadrada de $i é de $raiz <br>";
+}
